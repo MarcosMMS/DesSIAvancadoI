c67925369b688473fc1c73a88407007c623b68d4
MarcosMMS <105466751+MarcosMMS@users.noreply.github.com>
2024-08-20 23:27:51 +0000

    Pronto02

--- aula04/Program.cs
+++ aula04/Program.cs
@@ -4,9 +4,18 @@
         Console.WriteLine("Antes do metodo (Classe):" + objPessoa.ToString());
         AlteraNomeClasse(objPessoa);
         Console.WriteLine("Depois do metodo (Classe):"+ objPessoa.ToString());
+
+        PessoaStruct pessoaStruct = new PessoaStruct("Silva");
+        Console.WriteLine("Antes do metodo(struct): " + pessoaStruct.ToString());
+        AlteraNomeStruct(pessoaStruct);
+        Console.WriteLine("Depois do metodo Struct: " + pessoaStruct.ToString());
     }
 
     public static void AlteraNomeClasse(Pessoa objAux){
-        objAux.Nome = "Daniely";
+        objAux.Nome = "Marcos";
+    }
+
+    public static void AlteraNomeStruct(PessoaStruct objAux){
+        objAux.NomeStruct = "Moreira";
     }
 }
