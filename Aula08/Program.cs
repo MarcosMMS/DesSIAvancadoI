ab3f0dffecfbb4e2ecb67505ce2fac814e1b5c9d
MarcosMMS <105466751+MarcosMMS@users.noreply.github.com>
2024-09-03 23:23:53 +0000

    Aula dia 03/09

---
+++ Aula08/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Program
+{
+    // The Main method is the entry point of the application
+    public static void Main()
+    {
+        // Lista de Numeros
+        List<int> numeros = new List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        List<int> numerosPares = numeros.Where(n => n % 2 == 0).ToList();
+
+        // Numeros Pares
+        Console.WriteLine("Números Pares: ");
+        numerosPares.ForEach(n => Console.WriteLine(n));
+
+        // Frutas
+        List<string> frutas = new List<string> {"Maça", "Banana", "Manga", "Abacaxi", "Melancia", "Morango"};
+
+        List<string> frutasOrdenadas = frutas.OrderBy(f => f).ToList();
+
+        List<string> frutasComM = frutasOrdenadas.Where(f => f.StartsWith("M")).ToList();
+
+        Console.WriteLine("Frutas com M: ");
+        frutasComM.ForEach(f => Console.WriteLine(f));
+
+        List<Pessoa> pessoas = new List<Pessoa>{
+            new Pessoa("Marcos", 21),
+            new Pessoa("Miguel", 30),
+            new Pessoa("Rafael", 25),
+            new Pessoa("Luis", 25),
+            new Pessoa("Igor", 21),
+        };
+
+        List<Pessoa> pessoasFiltradas = pessoas.Where(p => p.Idade > 25).OrderBy(p => p.Nome).ToList();
+
+        Console.WriteLine("Pessoas com idade maior que 25: ");
+        pessoasFiltradas.ForEach(p => Console.WriteLine($"Nome: {p.Nome}, Idade: {p.Idade}"));
+    }
+}
--- Aula08/obj/Debug/net8.0/Aula08.AssemblyInfo.cs
+++ Aula08/obj/Debug/net8.0/Aula08.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula07")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0929cc30061dc2dbe83eefba6d9f8157282c44e")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula07")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula07")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula08")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
