68d3ad4f2d563cc50926395c6e0b637746baa481
MarcosMMS <105466751+MarcosMMS@users.noreply.github.com>
2024-09-24 23:16:02 +0000

    Venda-Aula11

--- DesenvolvimentoSistemas/Aula11/Models/Vendas.cs
+++ DesenvolvimentoSistemas/Aula11/Models/Vendas.cs
@@ -2,7 +2,26 @@ namespace Aula11.Models
 {
     public class Venda{
         public int id { get; set; }
-        public Cliente cliente { get; set; }
-        public Livro livro { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<Livro> Livros { get; set; }
+
+        public Venda(){
+            Cliente = new Cliente("",0);
+            Livros = new List<Livro>();
+        }
+
+        public Venda(Cliente cliente;){
+            Cliente = cliente;
+            Livros = new List<Livro>();
+        }
+
+        public void AdicionarLivro(Livro livro){
+            Livros.Add(livro);
+        }
+
+        public override string ToString(){
+            string livrosStr = string.Join(" , ", Livros.Select(livro=> livro.Nome));
+            return $"Compra de {Cliente.Nome}, Produtos: [{livrosStr}], Total: {CalcularTotal():C}";
+        }
     }
 }
