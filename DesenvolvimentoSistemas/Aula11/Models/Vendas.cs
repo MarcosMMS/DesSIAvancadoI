31398fe9e2575147881cb9be117ec1f8f9fe7c05
MarcosMMS <105466751+MarcosMMS@users.noreply.github.com>
2024-09-24 23:16:15 +0000

    Vendas-Aula11

--- DesenvolvimentoSistemas/Aula11/Models/Vendas.cs
+++ DesenvolvimentoSistemas/Aula11/Models/Vendas.cs
@@ -18,10 +18,29 @@ namespace Aula11.Models
         public void AdicionarLivro(Livro livro){
             Livros.Add(livro);
         }
+        
+        public decimal CalcularTotal(){
+        return Livros.Sum(l => l.Preco);
+        }
+
+        public void ListarLivrosMaisCaros(decimal precoMinimo){
+            var livrosCaros = Livros.Where(l => l.Preco > precoMinimo).OrderBy(l => l.Nome).ToList();
+            if(livrosCaros.Count > 0){
+                Console.WriteLine($"Livros com preco maior que {precoMinimo:C}");
+                livrosCaros.foreach(l => Console.WriteLine(l));
+            }else{
+                Console.WriteLine($"Nenhum livro com o preco maior que {precoMinimo:C}")
+            }
+        }
+
+        public void LivroEmOrdem(){
+            var livrosOrdenados = Livros.OrderBy(l => l.Nome).ToList();
+            livrosOrdenados.ForEach(l => Console.WriteLine(l));
+        }
 
         public override string ToString(){
-            string livrosStr = string.Join(" , ", Livros.Select(livro=> livro.Nome));
-            return $"Compra de {Cliente.Nome}, Produtos: [{livrosStr}], Total: {CalcularTotal():C}";
+            string livrosStr = string.Join(" , ", Livros.Select(l=> l.Nome));
+            return $"Venda para {Cliente.Nome}, Produtos: [{livrosStr}], Total: {CalcularTotal():C}";
         }
     }
 }
